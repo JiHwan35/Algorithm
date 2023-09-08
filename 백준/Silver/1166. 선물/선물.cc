f89f5f16c7e65988000e0f766d5d0b396f8c5ce0
JiHwan <93183216+JiHwan35@users.noreply.github.com>
2023-09-09 05:18:35 +0900

    [Silver III] Title: 선물, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1166. 선물/선물.cc
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int n = 0, l = 0, w = 0, h = 0;
+	double a = 0;
+
+	cin >> n >> l >> w >> h;
+
+	double low = 0,high = 1000000000,mid = 0;
+
+	for (int i = 0; i < 10000; i++)
+	{
+		mid = (low + high) / 2;
+		
+		if (n <= ((long long)(l / mid) * (long long)(w / mid) * (long long)(h / mid)))
+			low = mid;
+		else
+			high = mid;
+	}
+
+	cout << fixed;
+    cout.precision(9);
+
+	cout << low << endl;
+
+
+}
