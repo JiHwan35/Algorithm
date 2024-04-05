3dddbbedfb242e3cb1522e7609acf05cbbec8df2
JiHwan <93183216+JiHwan35@users.noreply.github.com>
2024-04-06 00:21:36 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.02 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(int n) {
+    vector<int> answer;
+    
+    for(int i = 0; i<n; i+=2)
+    {
+        answer.push_back(i+1);
+    }
+    return answer;
+}
