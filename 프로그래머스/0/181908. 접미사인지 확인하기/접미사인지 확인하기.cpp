23ecabf05e5b85002a56fb09486d41e6759d0663
JiHwan <93183216+JiHwan35@users.noreply.github.com>
2025-04-28 21:54:09 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.01 ms, Memory: 4.18 MB -BaekjoonHub

---
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string my_string, string is_suffix) {
+    int answer = 0;
+
+    if(my_string.size()<is_suffix.size())
+        return answer;
+    
+    if(my_string.substr(my_string.size()-is_suffix.size()) == is_suffix)
+       answer = 1;
+    
+    return answer;
+}
