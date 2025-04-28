52d74621a40f152a006562d516347ddefd9b1329
JiHwan <93183216+JiHwan35@users.noreply.github.com>
2025-04-28 22:09:01 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.cpp
+++ 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.cpp
@@ -4,7 +4,8 @@
 using namespace std;
 
 int solution(int num1, int num2) {
-    int answer = -1;
-    if(num1==num2) answer =1;
+    int answer = 1;
+    if(num1!=num2) 
+        answer =-1;
     return answer;
 }
