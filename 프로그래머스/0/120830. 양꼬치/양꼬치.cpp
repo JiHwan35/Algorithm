9d6e4219b60789b176b9c22632adf35f5951b34d
JiHwan <93183216+JiHwan35@users.noreply.github.com>
2025-04-28 22:16:20 +0900

    [level 0] Title: 양꼬치, Time: 0.01 ms, Memory: 4.44 MB -BaekjoonHub

--- 프로그래머스/0/120830. 양꼬치/양꼬치.cpp
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.cpp
@@ -4,10 +4,10 @@
 using namespace std;
 
 int solution(int n, int k) {
-    int answer = 0;
+
+    if(n>=10) 
+        k -= n/10;
     
-    if(n>=10) k -= n/10;
-    answer = 12000*n + 2000*k;
-    
-    return answer;
+    return 12000*n + 2000*k;
+ 
 }
