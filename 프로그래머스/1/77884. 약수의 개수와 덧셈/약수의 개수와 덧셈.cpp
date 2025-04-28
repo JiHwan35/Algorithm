0161d4fadb1b96000ad5dd997c23bf0beb7fa497
JiHwan <93183216+JiHwan35@users.noreply.github.com>
2025-04-28 22:50:28 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 1.20 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.cpp
@@ -0,0 +1,34 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+
+int divisor(int n){
+    int num = 0;
+    for(int i = 1; i<=n; i++)
+        if(n%i == 0)
+            num++;
+    
+    return num;
+}
+
+
+int solution(int left, int right) {
+    int answer = 0;
+    
+    for( int i = left; i<=right; i++)
+    {
+        int d = divisor(i);
+        if(d %2 == 0)
+            answer += i;
+        else
+            answer-=i;
+        
+        
+        
+        
+    }
+    
+    return answer;
+}
