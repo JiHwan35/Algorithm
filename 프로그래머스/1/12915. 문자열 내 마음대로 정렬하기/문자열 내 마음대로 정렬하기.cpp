33935eab61b977751e7820185c1a01f14b7192b9
JiHwan <93183216+JiHwan35@users.noreply.github.com>
2025-04-28 22:59:56 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 0.09 ms, Memory: 4.22 MB -BaekjoonHub

---
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int idx = 0;
+
+bool isgreater(string s1, string s2){
+    if(s1[idx] == s2[idx])
+        return s1<s2;
+    else
+        return s1[idx]<s2[idx];
+    
+}
+vector<string> solution(vector<string> strings, int n) {
+    vector<string> answer;
+    idx = n;
+    
+    sort(strings.begin(),strings.end(),isgreater);
+    
+    answer = strings;
+    
+    return answer;
+}
