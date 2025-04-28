f4dc66a7a3fea2416e65da38ced5ab01dda19875
JiHwan <93183216+JiHwan35@users.noreply.github.com>
2025-04-28 21:41:05 +0900

    [level 0] Title: 코드 처리하기, Time: 0.51 ms, Memory: 3.69 MB -BaekjoonHub

---
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.cpp
@@ -0,0 +1,35 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string code) {
+    string answer = "";
+    string ret= "";
+    int mode = 0;
+    
+    for(int i = 0; i<code.length(); i++){
+        if(mode == 0){
+            if(code[i] == '1')
+                mode = 1;
+            else if(i%2==0)
+                ret.push_back(code[i]);
+        }
+        else{
+            if(code[i] == '1')
+                mode = 0;
+            else if(i%2==1)
+                ret.push_back(code[i]);
+        }
+            
+    }    
+    
+    if(ret.empty()){
+        answer = "EMPTY";    
+        return answer;
+    }
+    
+    answer = ret;
+    
+    return answer;
+}
