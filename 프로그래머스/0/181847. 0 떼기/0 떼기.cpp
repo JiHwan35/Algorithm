762396f8f7953685aef1aa7d97ab7964855a1d8e
JiHwan <93183216+JiHwan35@users.noreply.github.com>
2025-04-28 22:33:17 +0900

    [level 0] Title: 0 떼기, Time: 0.02 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string n_str) {
+    string answer = "";
+    
+    answer = to_string(stoi(n_str));
+    
+    return answer;
+}
