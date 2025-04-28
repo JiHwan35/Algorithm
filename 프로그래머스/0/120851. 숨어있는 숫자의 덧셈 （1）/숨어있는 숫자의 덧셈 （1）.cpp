c90d624b5dc4cd3e0ca3d620f679d838511a718e
JiHwan <93183216+JiHwan35@users.noreply.github.com>
2025-04-28 22:22:15 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.01 ms, Memory: 4.18 MB -BaekjoonHub

--- 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.cpp
+++ 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.cpp
@@ -6,9 +6,10 @@ using namespace std;
 int solution(string my_string) {
     int answer = 0;
     
-    for(char c : my_string){
-        if(c >= '1' && c <='9') answer += c-48;
-        }
+    for(char c : my_string)
+        if(c >= '1' && c <='9') 
+            answer += c-48;
+        
     
     return answer;
 }
