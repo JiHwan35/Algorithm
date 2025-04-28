a24ae8b5237322b465655bd2e522a275c91a89c3
JiHwan <93183216+JiHwan35@users.noreply.github.com>
2025-04-28 22:34:22 +0900

    [level 0] Title: l로 만들기, Time: 0.76 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string myString) {
+    string answer = "";
+    
+    for(auto c : myString)
+    {
+        if(c<'l')    
+            answer+= 'l';
+        else
+            answer+=c;
+    }
+    
+    return answer;
+}
