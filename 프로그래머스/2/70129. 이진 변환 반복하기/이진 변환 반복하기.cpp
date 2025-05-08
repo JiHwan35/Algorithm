60db7cbe275d881ac6cdc0a9de41976156872176
JiHwan <93183216+JiHwan35@users.noreply.github.com>
2025-05-08 19:43:29 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 1.15 ms, Memory: 3.87 MB -BaekjoonHub

---
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.cpp
@@ -0,0 +1,43 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+vector<int> solution(string s) {
+    vector<int> answer;
+    int sum = 0;
+    int num = 0;
+    
+    while(s != "1"){
+        string tmp = "";
+
+        for(char c :  s){
+            if(c == '1')
+                tmp+=c;
+            else
+                sum++;
+        }
+        cout<<sum<< " "<<tmp<<endl;
+        
+        int n = tmp.size();
+        string bn = "";
+        
+        while(n>0){
+            if( n%2 == 0 )
+                bn ='0'+bn;
+            else
+                bn = '1'+bn;
+
+            n/=2;
+        }
+
+        s = bn;
+        num++;
+    }
+        answer.push_back(num);
+        answer.push_back(sum);
+
+        return answer;
+    }
